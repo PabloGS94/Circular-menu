9efcf5a04967bbe0016dca8cc2a88f06c018e054
PabloGS94 <pablogiles7@gmail.com>
2021-01-23 12:44:59 +0200

    three menus added, event listener in all three

--- menu.js
+++ menu.js
@@ -1,56 +1,67 @@
+// Select here the button that opens the menu and the wrapper element of the menu screen
 const openMenu =  document.querySelector('#open-menu-btn');
 const menu = document.querySelector('#menu');
-
-// Get all items on the menu
-const listItems = document.querySelectorAll('li');
-// Transform listItems from a nodeList to an array( necessary in order to reverse it)
-const listItemsArr = jQuery.makeArray(listItems);
-// Reverse the array(necessary for arrow up functionality to prevent eternal loop through items)
-const reversedArr = listItemsArr.reverse()
-
 //Show menu
 openMenu.addEventListener('click', ()=>{
     showMenu();
 })
 
+// Get all lists
+const firstList = document.querySelectorAll('#first-list li');
+const secondList = document.querySelectorAll('#second-list li');
+const thirdList = document.querySelectorAll('#third-list li');
+// Transform each list from a nodeList to an array( necessary in order to reverse it)
+const secondListItemsArr = jQuery.makeArray(secondList);
+const thirdListItemsArr = jQuery.makeArray(thirdList);
+// Reverse each array(necessary for arrow up functionality to prevent eternal loop through items)
+const firstReversedArr = firstListItemsArr.reverse();
+const seconReversedArr = secondListItemsArr.reverse();
+const thirdReversedArr = thirdListItemsArr.reverse();
+
+animateList(firstList);
+animateList(secondList);
+animateList(thirdList);
+
 function showMenu(){
     menu.style.display = 'block';
-    listItems.forEach(function(element){
-        $(element).fadeIn('slow')
-    });
-    //Animate circles
+    //Select background  menu elements
     const firstCircle = document.querySelector('#first-circle');
     const secondCircle = document.querySelector('#second-circle');
     const thirdCircle = document.querySelector('#third-circle');
     const greyBackground = document.querySelector('#grey-background');
     const nav = document.querySelector('#nav')
+    //Animate menu background
     $(secondCircle).animate({
         left:'50%'
     },1200);
     $(thirdCircle).animate({
-        left:'20%'
+        left:'15%'
     },1400);
     $(greyBackground).animate({
         left:0
     },1000)    
     $(firstCircle).animate({
-        left:'40%'
-    },2000);
-    
+        left:'45%'
+    },1400);    
     setTimeout(()=>{
     nav.style.background = 'none';
     },600)
 }
-
-// Adds event listener for arrow keys to navigate the menu
-// document.onkeydown = checkKey;
-// Iterate through all elements on the menu and add a click event listener to display that item in the center and move the rest accordingly
-listItems.forEach(function(element){
-    element.addEventListener('click', ()=>{ 
-        //Avoids spam on links, prevents malfunction
-        setTimeout(listenForClick,200,element);
-    })
-});
+function animateList(list){
+    // Iterate through all elements on the menu and add a click event listener to display that item in the center and move the rest accordingly
+    list.forEach(function(element, index){
+        // Style every element to the bottom right initially
+        element.style.top = 200*index + 'px'; 
+        element.style.left = 100*index + 'px'; 
+        if(element.getAttribute('main') == 'true'){
+            //Display arrow icon
+            element.childNodes[0].style.visibility = 'visible';
+        }
+        element.addEventListener('click', ()=>{ 
+            listenForClick(element)        
+        })
+    });
+}
 
 // Main function that determines the element that will be displayed in the center(selected element)
 function listenForClick(element){
@@ -87,103 +98,66 @@ function listenForClick(element){
             //Set Selected item to Main            
             element.setAttribute('main', 'true');
             element.classList.add('main');
-
+            //Display arrow icon
+            element.childNodes[0].style.visibility = 'visible';
         }
 }
 
-// $(document).keydown(function(event){
-//     $flag=false;
-//     var keycode = (event.keyCode ? event.keyCode : event.which);
-
-//     if(keycode === 40){
-//         if(!$flag){
-//             $flag=true;
-//         }
-//     }else if(keycode === 38){
-//         $flag=true;
-
-//     }
- 
-// });
-var down = {};
-
-$(document).keydown(function(event){
-    let runningAnimation = false
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if  (runningAnimation = true){
+//Listens for arrow keys function and if its being pressed down or just pressed once
+function listonForArrows(list){
+    var down = {};
+    $(document).keydown(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
         if(keycode == '40'){
             if (down['40'] == null) { // first press
-                
-                runningAnimation = true;
-                setTimeout(()=>{
-                    // Arrow up was pressed, move to item above
-                    reversedArr.forEach(function(element, index){
-                        // Search for the Main item
-                        if(element.hasAttribute('main')){
-                            // If the Main item is the last one then we return null
-                            if (reversedArr[index-1] === undefined) {
-                                return null;
-                            }
-                            // Else we move every item upwards and we set the next item as the Main
-                            else{
-                            moveItemsUp(element);
-                            reversedArr[index-1].setAttribute('main', 'true');
-                            reversedArr[index-1].classList.add('main')
-                            }
+                // Arrow up was pressed, move to item above
+                reversedArr.forEach(function(element, index){
+                    // Search for the Main item
+                    if(element.hasAttribute('main')){
+                        // If the Main item is the last one then we return null
+                        if (reversedArr[index-1] === undefined) {
+                            return null;
                         }
-                    });
-                },200)
-                
-                runningAnimation = false;
+                        // Else we move every item upwards and we set the next item as the Main
+                        else{
+                        moveItemsUp(element);
+                        reversedArr[index-1].setAttribute('main', 'true');
+                        reversedArr[index-1].classList.add('main')                    
+                        reversedArr[index-1].childNodes[0].style.visibility = 'visible';
+                        }
+                    }
+                });
                 down['40'] = true; // record that the key's down
             }
         }else if(keycode === 38){
             if(down['38'] == null){
-                runningAnimation = true;
-                setTimeout(()=>{
-                    // Arrow down was pressed, move to item below
-                    listItems.forEach(function(element, index){
-                        // Search for the Main item
-                        if(element.hasAttribute('main')){
-                            // If the Main item is the last one then we return null
-                            if (listItems[index-1] === undefined) {
-                                return null;
-                            }
-                            // Else we move every item downwards and we set the next item as the Main
-                            else{                
-                                moveItemsDown(element);           
-                                listItems[index-1].setAttribute('main', 'true');
-                                listItems[index-1].classList.add('main');
-            
-                            }
+                // Arrow down was pressed, move to item below
+                firstList.forEach(function(element, index){
+                    // Search for the Main item
+                    if(element.hasAttribute('main')){
+                        // If the Main item is the last one then we return null
+                        if (listItems[index-1] === undefined) {
+                            return null;
                         }
-                    });
-
-                },200)
-                runningAnimation = false;
-                down['38'] == true;
+                        // Else we move every item downwards and we set the next item as the Main
+                        else{                
+                            moveItemsDown(element);           
+                            firstList[index-1].setAttribute('main', 'true');
+                            firstList[index-1].classList.add('main');           
+                            firstList[index-1].childNodes[0].style.visibility = 'visible';    
+                        }
+                    }
+                });
+            down['38'] == true;
             }
         }
-    }else{
-        console.log('animation running');
-    }
-});
+    });
+    $(document).keyup(function(event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        down[keycode] = null;
+    });
 
-$(document).keyup(function(event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    down[keycode] = null;
-});
-//Function that determines if the arrow up or arrow down was pressed and move elements accordingly
-// function checkKey(e) {
-
-//     e = e || window.event;
-
-//     if (e.keyCode === 40) {
-//     }
-//     else if (e.keyCode === 38) {
-        
-//     }
-// }
+}
 
 
 /*
@@ -213,6 +187,8 @@ function moveItemsUp(item,timesToMove = 0){
     // We remove the atribute and styling of the previous Main element
     item.removeAttribute('main');
     item.classList.remove('main');
+    // Hide arrow icon
+    item.childNodes[0].style.visibility = 'hidden';
 }
 
 
@@ -243,6 +219,8 @@ function moveItemsDown(item,timesToMove = 0){
     // We remove the atribute and styling of the previous Main element
     item.removeAttribute('main');
     item.classList.remove('main');
+    //  Hide arrow icon
+    item.childNodes[0].style.visibility = 'hidden';
 }
 
 
